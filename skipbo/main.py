5f15cad637d1744a2fea52dc0942dc2b811ceaf0
Parsa Sotoodeh <parsa@MacBook-Pro.local>
2023-05-12 13:21:50 -0400

    added deck class

--- skipbo/main.py
+++ skipbo/main.py
@@ -1,6 +1,21 @@
+import random 
+
 Class Card:
     def __init__(self,value,color):
         self.value = value
         self.color = color
     def __str__(self):
         return f"{self.value} of {self.suit}"
+
+Class deck:
+    def __init__(self,cards):
+        self.cards = cards
+    
+    def shuffle(self):
+        return(random.shuffle(self.cards))
+    def draw(self):
+        return(self.cards[0])
+    def is_empty(self):
+        if self.cards:
+            return False
+        return True
